ef3fcf8f106ac72803af06eb52ef8939ab1c8c3b
BuildTools <unconfigured@null.spigotmc.org>
2023-03-22 17:48:48 +0400

    some edits

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from PIL import Image
 st.set_page_config(page_title='Cat Breed Classifier', layout='centered')
 
 st.title("Cat Breed Classifier")
-st.subheader("Upload your cat image or be doomed")
+st.subheader("Upload your cat image")
 
-cat = st.sidebar.file_uploader("Upload yo cat")
+cat = st.sidebar.file_uploader("Upload your cat")
 
 if cat:
     st.image(cat)
     image = Image.open(cat)
     img_array = np.array(image)
-    st.subheader("your cat is "+ get_prediction(img_array)[0])
+    st.subheader("Your cat breed is "+ get_prediction(img_array)[0].lower())
 
     
 
